558a707c633432a973f8a884270d89f21324b995
Luki Centuri <drybutcool@gmail.com>
2018-10-03 12:31:56 +0700

    Fix Style and Script:

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 /* write your cool javascript code here */
+let counter = document.querySelector('#counter');
+
+document.querySelector('#inc').onclick = function(){
+    counter.value = +counter.value - 1;
+}
+
+document.querySelector('#dec').onclick = function(){
+    counter.value = +counter.value + 1;
+}
